7f13566a6884e8bcefa6f73e793efdde077d3455
jordanlanham52 <jordanlanham52@gmail.com>
2025-02-18 15:52:08 -0700

    t

--- LanhamAssignment3.py
+++ LanhamAssignment3.py
@@ -1,6 +1,6 @@
 '''
 Assignment 3 CYBV 312
-Jordan, Lanham
+Jordan Lanham
 Due on 2/23/25
 '''
 import time
@@ -14,32 +14,19 @@ def generate_hash(password):
     return 
 
 def brute_force_password(target_hash, charset, max_length):
-    """
-    Attempts to discover the password for a given hash using brute force.
-    Parameters:
-    - target_hash (str): The hash digest of the target password.
-    - charset (str): The set of characters to use for generating password combinations.
-    - max_length (int): The maximum length of the password to attempt.
-
-    Returns:
-    - str: The discovered password if found, or None if not found.
-    """
     # Iterate through all possible lengths up to max_length
     for length in range(1, max_length + 1):
         # Generate all combinations of the given length
         for combination in itertools.product(charset, repeat=length):
             # Join the combination to form a password
-            potentialPassword = combination
+            potentialPassword = "".join(combination)
             # Generate the hash of the candidate password
-            potentialHash = hashlib.md5(str(potentialPassword).encode())
+            potentialHash = hashlib.md5(potentialPassword.encode())
+            potentialHash = potentialHash.hexdigest()
             # Check if the hash matches the target hash
             if potentialHash == target_hash:
-                print(f"The password is {potentialPassword.hexdigest()}") 
+                print(f"The password is {potentialPassword} with the hash {potentialHash}") 
                 break
-            else:
-                print("Trying new password")
-
-
     return None  # Password not found within the given constraints
 
 def defineRT(charset, min_lenth, max_length):
@@ -55,19 +42,21 @@ def defineRT(charset, min_lenth, max_length):
     - dict: The dictionary composed of pairs <md5 hash digest, password>
     """    
     rainbowTable = {}      
-    #more code to add
-    pass
+    for length in range(min_lenth, max_length):
+        for combination in itertools.product(charset, repeat=length):
+            password = "".join(combination)
+            hash = hashlib.md5(password.encode()).hexdigest()
+            rainbowTable[hash] = password
     return rainbowTable
-            
+        
 def displayRT(rainbowTable):
-    '''
-    Display the size of the rainbow table and then display ten rows in a pretty table
-    Parameters:
-    - rainbowTable (dict) : The rainbow table to be displayed 
-    Returns:
-    - None
-    '''
-    pass
+    print(f"Rainbow table size: {len(rainbowTable)} entries")
+    table = PrettyTable(["MD5 Hash", "Password"])
+    for i, (hash_value, password) in enumerate(rainbowTable.items()):
+        if i >= 10:  
+            break
+        table.add_row([hash_value, password])
+    print(table)
     
     
 def main():
@@ -78,20 +67,22 @@ def main():
     
     #Modify the below code to test brute_force_password and document the time to discover the password
     charset = 'ab12!@'
+    start = time.time()
     found = brute_force_password(target, charset, 8)
-
+    end = time.time()
+    print(f"It took {end-start} seconds to find the password using brute force")
     #Modify the below code to test defineRT and document the time to construct the rainbow table
+    start = time.time()
     rt = defineRT(charset, 4, 6)
-    
+    end = time.time()
+    print(f"It took {end-start} seconds to generate the table")
     #Write more code to check how many seconds used to discover the password given the target hash using the rainbow table 
     start = time.time()
-    times = 100000
-    for _ in range(times):
-        brute_force_password
+    rtPassword = rt.get(target, None)
     end = time.time()
-    print(f"It took {start-end} seconds and {times} tries to find the password")
+    print(f"It took {end-start} seconds to find the password using rainbow tables")
     #Call displayRT to test the function definition
-      
+    displayRT(rt)
 
 if __name__=='__main__':
     main()
