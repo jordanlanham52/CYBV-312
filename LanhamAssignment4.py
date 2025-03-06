3158aa5bdc564653619c1bbd4e66daec64c3dc1f
jordanlanham52 <jordanlanham52@gmail.com>
2025-03-06 16:28:58 -0700

    final fix

--- LanhamAssignment4.py
+++ LanhamAssignment4.py
@@ -47,31 +47,32 @@ def get_filemetadata(filePath):
     Given a file path, collect the metadata that describe the file
     
     Parameters:
-    - filepath(str): the file path used to access the file
+    - filePath (str): the file path used to access the file
     
     Returns:
-    tuple: the return tuple is composed of the abosolute path, 
-           file size, hashvalue of the file content, a list of MAC times
-           if the file can be opened 
-    None: otherwise
+    tuple: (absolute path, file size, hash value, formatted MAC times)
     '''
     try:
         absPath  = os.path.abspath(filePath)  # Get absolute file path
         fileSize = os.path.getsize(absPath)  # Get file size
         hashValue = hash_filecontent(absPath)  # Compute hash
         
-        # Get MAC times (Modification, Access, Creation)
+        # Get raw MAC times
         mTime = os.path.getmtime(absPath)  # Last modification time
         aTime = os.path.getatime(absPath)  # Last access time
-        cTime = os.path.getctime(absPath)  # File creation time (on Windows) or last status change (on Unix)
+        cTime = os.path.getctime(absPath)  # File creation time (Windows) or status change (Unix)
 
-        return (absPath, fileSize, hashValue, mTime, aTime, cTime)
+        # Convert to human-readable format
+        readable_mTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(mTime))
+        readable_aTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(aTime))
+        readable_cTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(cTime))
+
+        return (absPath, fileSize, hashValue, readable_mTime, readable_aTime, readable_cTime)
     
     except Exception as err:
         print(f"Error: {err}")
-        return None
-
-
+        return None   
+   
 # main function
 def main():
     print("Current working directory: ",Path.cwd())
@@ -99,8 +100,7 @@ def main():
             if metadata:
                 absPath, fileSize, hashValue, mTime, aTime, cTime = metadata
                 tbl.add_row([absPath, fileSize, hashValue, mTime, aTime, cTime])
-
-    
+                
             #You will need to add the variables that you stored the MAC and HASH
             #Code to add data to the pretty table
     
@@ -124,7 +124,7 @@ def main():
     
     ''' Open the CSV File and write the contents'''
     with open(csvFileName, 'w') as outFile:
-        csvOut = tbl.get_csv_string(sortby="File Path")
+        csvOut = tbl.get_csv_string(sortby="Created Time")
         outFile.write(csvOut)
     
     print("\nScript-End\n")
